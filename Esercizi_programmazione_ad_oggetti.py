b76826225a89adfc1c9a71777e208abf5368a7e1
Lamberto Martini <89637786+Lambo95@users.noreply.github.com>
2026-04-10 20:28:54 +0200

    Update Esercizi_programmazione_ad_oggetti.py

--- Esercizi_programmazione_ad_oggetti.py
+++ Esercizi_programmazione_ad_oggetti.py
@@ -629,3 +629,84 @@ print(cc1)
 conto_fuso = cc1 + cc2
 print(conto_fuso) """
  
+""" from abc import ABC, abstractmethod 
+import math
+class Forma(ABC):
+    
+    @abstractmethod
+    def perimetro(self):
+        pass
+    
+    @abstractmethod
+    def area(self):
+        pass
+    
+    def __eq__(self, altra):
+         return math.isclose(self.area(), altra.area())
+    
+    def __str__(self):
+        return f"{self.__class__.__name__} - Area: {self.area():.2f}, Perimetro: {self.perimetro():.2f}"
+    
+class Rettangolo(Forma):
+    def __init__(self, base, altezza):
+        self.base = base
+        self.altezza = altezza
+    
+    def perimetro(self):
+        return 2 * (self.base + self.altezza)
+    
+    def area(self):
+        return self.base * self.altezza
+    
+class Cerchio(Forma):
+    def __init__(self, raggio):
+        self._raggio = raggio
+    
+    @property
+    def raggio(self):
+        return self._raggio
+    
+    def diametro(self):
+        return self._raggio * 2
+    
+    def area(self):
+        return math.pi * self._raggio ** 2
+    
+    def perimetro(self):
+        return 2 * math.pi * self._raggio
+    
+class SommaFigure(Forma):
+    def __init__(self, *figure):
+        self.figure = figure
+    
+    def perimetro(self):
+        return sum([f.perimetro() for f in self.figure])
+    
+    def area(self):
+        return sum([f.area() for f in self.figure])
+    
+    def __str__(self):
+        return f"SommaFigure - Area: {self.area():.2f}, Perimetro: {self.perimetro():.2f}"
+    
+Forma.__add__ = lambda self, altra: SommaFigure(self, altra)
+
+rett= Rettangolo(4, 6)
+cerchio = Cerchio(3)
+
+print("=== Figure iniziali ===")
+print(rett)
+print(f"Diametro del cerchio: {cerchio.diametro()}")
+print(cerchio)
+
+print("\n=== Polimorfismo ===")
+figure = [rett, cerchio]
+for figura in figure:
+    print(figura)
+
+print("\n=== Confronto aree ===")
+print(rett == cerchio)
+
+print("\n=== Combinazione con + ===")
+somma = rett + cerchio
+print(somma)
+         """
